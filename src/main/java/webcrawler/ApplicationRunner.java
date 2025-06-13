fd50f802a53ea652d7f921b23d8cc8c38ddbbbb4
Keri Southwood-Smith <keri.southwood@gmail.com>
2025-06-13 16:57:21 -0400

    Initial source files

---
+++ src/main/java/webcrawler/ApplicationRunner.java
@@ -0,0 +1,7 @@
+package webcrawler;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        new WebCrawler();
+    }
+}
---
+++ src/main/java/webcrawler/WebCrawler.java
@@ -0,0 +1,13 @@
+package webcrawler;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class WebCrawler extends JFrame {
+    public WebCrawler() throws HeadlessException {
+        super("Web Crawler");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+    }
+}
